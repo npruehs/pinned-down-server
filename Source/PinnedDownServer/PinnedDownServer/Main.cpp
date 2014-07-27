1969a84b7819df6556107e5b4913ddcd1fc276ce
Nick Prühs <dev@npruehs.de>
2014-07-27 20:26:38 +0200

    ADDED: Project - Initial files.

---
+++ Source/PinnedDownServer/PinnedDownServer/Main.cpp
@@ -0,0 +1,15 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+	int i;
+	cin >> i;
+	return 0;
+}
